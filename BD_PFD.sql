2c4ab2930bf349a69a98e2e6788ba4896d367ab8
Oliver Alexis Botello Marias <bmoo130768@upemor.edu.mx>
2016-10-11 23:46:45 -0500

    Con el archivo de BD
    Se agrego el archivo SQL de la BD,  en este solo se encuentra el comando
    para crear la BD, la conexion entre el BD y el proyecto aun no se
    realiza

---
+++ BD_PFD.sql
@@ -0,0 +1,21 @@
+
+#############################################
+#                Desarrollador              #
+# Nombre: Oliver Alexis Botello Marias      #
+#                                           #
+# Correo(s):                                #
+#	Personal: OliverAlxBotello@gmail.com    #
+#	Institucional: bmoo130768@upemor.edu.mx #
+#                                           #
+#############################################
+
+# ARCHIVO
+# 	Nombre: BD_PFD.sql
+# 	Fecha de creacion: 10/Octubre/2016
+# 	Fecha de ultima modificacion: 10/Octubre/2016 
+
+# DESCRIPCION:
+#	Script de la base de datos del sistema PFD;
+
+#Crear base dedatos;
+CREATE DATABASE BD_PFD;
